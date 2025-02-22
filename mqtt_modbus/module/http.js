b7c2933e544794cf145129e753b384623abc19f7
Agustin Negrin <agustinng14@gmail.com>
2025-02-22 18:12:22 -0400

    [feature] Enhance HTTP module message parsing and handling

--- mqtt_modbus/module/http.js
+++ mqtt_modbus/module/http.js
@@ -31,13 +31,40 @@ const initialize = async () => {
     const proTopic = topic.split("/");
     const sede = proTopic.shift();
     const parametro = proTopic.shift();
-    const toSend = `http://tomcat:8080/httpds?&${parametro}=${encodeURI(
-      message ? message.toString() : ""
-    )}&__device=${sede}&__time=${dayjs()
-      .subtract(4, "hours")
-      .format("YYYYMMDDHHMMSS")}`;
-    console.log("🚀 ~ toSend:", toSend);
-    return axios.get(toSend);
+    let correctedMessage = message ? message.toString() : "";
+    correctedMessage = ["0", "false", "falso", "False", "Falso"].includes(
+      correctedMessage
+    )
+      ? "0"
+      : correctedMessage;
+    correctedMessage = ["1", "true", "verdadero", "True", "Verdadero"].includes(
+      correctedMessage
+    )
+      ? "1"
+      : correctedMessage;
+    try {
+      const jsonMessage = JSON.parse(correctedMessage);
+      if (typeof jsonMessage !== "object") {
+        throw new Error("El mensaje no es un objeto valido");
+      }
+      for (const key in jsonMessage) {
+        const toSend = `http://tomcat:8080/httpds?&${key}=${encodeURI(
+          jsonMessage[key]
+        )}&__device=${sede}&__time=${dayjs()
+          .subtract(4, "hours")
+          .format("YYYYMMDDHHMMSS")}`;
+        console.log("🚀 ~ toSend:", toSend);
+        return axios.get(toSend);
+      }
+    } catch (error) {
+      const toSend = `http://tomcat:8080/httpds?&${parametro}=${encodeURI(
+        correctedMessage
+      )}&__device=${sede}&__time=${dayjs()
+        .subtract(4, "hours")
+        .format("YYYYMMDDHHMMSS")}`;
+      console.log("🚀 ~ toSend:", toSend);
+      return axios.get(toSend);
+    }
   });
 
   ////////// HTTP Functions //////////////
