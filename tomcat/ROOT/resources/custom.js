7c2a99d563b8c20d865b15a0e0aa5c8c1d7f313a
Agustin Negrin <agustinng14@gmail.com>
2025-05-07 18:59:04 -0400

    [refactor] Update styles in common.css and custom.css for improved layout and visual consistency; comment out unused JavaScript in custom.js.

--- tomcat/ROOT/resources/custom.js
+++ tomcat/ROOT/resources/custom.js
@@ -1,3 +1,3 @@
-$(function () {
-  $("#mainHeader").append('<div id="bannerText">Sistema de Monitoreo</div>');
-});
+// $(function () {
+//   $("#mainHeader").append('<div id="bannerText">Sistema de Monitoreo</div>');
+// });
